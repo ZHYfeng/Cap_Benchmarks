4d13e166bfee178a560a7608ad273b1cf3ef12c1
HY <1129719536@qq.com>
2015-11-26 12:24:00 +0800

    统计maple的内存

--- maple/do.sh
+++ maple/do.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
+getmemory(){
+	sleep 0.01
+	for file in `ls /proc/`
+	do
+		if [ -d /proc/$file ]
+			then
+			cmd=`awk '{ print $1; }' /proc/${file}/comm`
+			if [ $cmd == "maple" ]
+				then
+				temp=`awk -F" " '{ print ($2-$3)*4; }' /proc/${file}/statm`
+				mem=0
+				while [[ -n $temp ]]
+				do
+					if [ $temp -gt $mem ]
+						then
+						mem=$temp
+					fi
+					temp=`awk -F" " '{ print ($2-$3)*4; }' /proc/${file}/statm`
+				done
+				sleep 0.5
+				echo " $mem" >> ${all}/DATE.txt
+				break
+			fi
+		fi
+	done
+}
+
 getmaple(){
 	lines=`awk 'END{ print NR; }' ./result.txt`
 	awk -F" " '{ if( NR == '$lines'-6 ){ printf("%s%s%s ",$3,$4,$5); } else if( NR >= '$lines'-4 ){ printf("%s ",$3); } }' ./result.txt >> ${all}/DATE.txt
-	echo "" >> ${all}/DATE.txt
 }
 
 recurrence(){
@@ -16,11 +42,13 @@ recurrence(){
 		elif [ $file == "test.sh" ]
 			then
 			make
-			for((i=0;i<100;i++))
+			for((i=0;i<1;i++))
 			do 
+				getmemory &
 				./$file
 				pwd | awk -F"/" '{ printf("%s ",$NF); }' >> ${all}/DATE.txt
 				getmaple
+				sleep 1
 				rm coverage iroot.db memo.db pct.histo sinfo.db sinst.db stat.out test.histo result.txt
 			done
 		fi
